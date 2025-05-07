eb57761c842e693b3fd5cda14b5e235c94f275aa
Victor Rodriguez <188802056+victor-rodriguez71@users.noreply.github.com>
2025-05-06 20:56:19 -0600

    operator functions added. operate function added to choose and run functions

---
+++ script.js
@@ -0,0 +1,53 @@
+let sum = 0
+
+function add (num1, num2) {
+   let sum = num1 + num2;
+   console.log(sum)
+   return sum
+}
+
+function subtract(num1, num2) {
+    let sum = num1 - num2;
+    console.log(sum)
+}
+
+function multiply(num1, num2) {
+    let sum = num1 * num2;
+    console.log(sum)
+}
+
+function divide(num1, num2) {
+    let sum = num1 / num2;
+    console.log(sum)
+}
+
+function operate() {
+    let operator = prompt("Enter a function: Add, Subtract, Multiply, Divide").toLowerCase();
+
+    if (operator === "add") {
+        let num1 = parseInt(prompt("Enter your first number: "));
+        let num2 = parseInt(prompt("Enter your second number: "));
+        add(num1, num2);
+        return sum
+    } else if (operator === "subtract") {
+        let num1 = parseInt(prompt("Enter your first number: "));
+        let num2 = parseInt(prompt("Enter your second number: "));
+        subtract(num1, num2);
+        return sum
+    } else if (operator === "multiply") {
+        let num1 = parseInt(prompt("Enter your first number: "));
+        let num2 = parseInt(prompt("Enter your second number: "));
+        multiply(num1, num2);
+        return sum
+    } else if (operator === "divide") {
+        let num1 = parseInt(prompt("Enter your first number: "));
+        let num2 = parseInt(prompt("Enter your second number: "));
+        divide(num1, num2);
+        return sum
+    } else {
+        prompt("Please enter a valid response")
+        operate();
+    }
+}
+
+operate()
